50844a929ab9c6f3fed19a6c380b7b842eb98cb3
Gulistane Raza <gulistaneraza01@gmail.com>
2025-08-26 17:26:36 +0530

    first commit

---
+++ client/src/constants.js
@@ -0,0 +1,5 @@
+export const BASE_URL = ""
+export const BACKEND_URL = "https://sparktech-ecommerce-youtube.onrender.com"
+export const PRODUCTS_URL = "/api/products"
+export const ORDERS_URL = "/api/orders"
+export const USERS_URL = "/api/users"
